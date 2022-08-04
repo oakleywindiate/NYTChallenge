7817b230be009b2ae398c6e93e211e726da2a63b
Oakley Windiate <oakley.windiate@gmail.com>
2022-08-04 11:23:13 -0600

    Create details page and add functionality to go back

--- src/Components/ArticleContainer.js
+++ src/Components/ArticleContainer.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import Article from './Article'
 import Details from './Details'
+import Article from './Article'
 
 export default function ArticleContainer({ articles }) {
     const [allArticles, setAllArticles] = useState([])
     const [details, setDetails] = useState([])
     const [articleTitle, setArticleTitle] = useState("")
 
-// abstract
-// byline
-// created_date
-// des_facet
-// geo_facet
-// item_type
-// kicker
-// material_type_facet
-// multimedia
-// org_facet
-// per_facet
-// published_date
-// section
-// short_url
-// subsection
-// title
-// updated_date
-// uri
-// url
 
 useEffect(() => {
     setAllArticles(articles)
@@ -60,9 +41,45 @@ const findArticle = (title) => {
         )
       })
 
-  return (
+      const detailsArticle = details.map((article) => {
+        return (
+            <div>
+                <Details
+                abstract={article.abstract}
+                byline={article.byline}
+                created_date={article.created_date}
+                title={article.title}
+                section={article.section}
+                subsection={article.subsection}
+                multimediaCaption={article.multimedia[0].caption}
+                multimediaCopyright={article.multimedia[0].copyright}
+                multimediaUrl={article.multimedia[1].url}
+                url={article.url}
+                uri={article.uri}
+                item_type={article.item_type}
+                updated_date={article.updated_date}
+                published_date={article.published_date}
+                material_type_facet={article.material_type_facet}
+                kicker={article.kicker}
+                des_facet={article.des_facet}
+                org_facet={article.org_facet}
+                per_facet={article.per_facet}
+                geo_facet={article.geo_facet}
+                multimedia={article.multimedia}
+                short_url={article.short_url}
+                />
+                <button onClick={() => {
+                    setArticleTitle("")
+                }}>BACK</button>
+            </div>
+        )
+      })  
+
+  return ( 
     <section>
-        <div>{singularArticle}</div>
+        {!articleTitle && <div>{singularArticle}</div>}
+        {articleTitle && <div>{detailsArticle}</div>}
     </section>    
   )
 }
+
